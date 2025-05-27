74d20f36582eb2aab21d3a0003025c34319359ad
Knz <dimapodschibiakin@yandex.ru>
2025-05-27 16:19:03 +0700

    import ingredients

--- itog/api/urls.py
+++ itog/api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from profiles.views import CustomUserViewSet
+from posts.views import IngredientViewSet
+
 
 router = DefaultRouter()
 router.register('users', CustomUserViewSet)
+router.register('ingredients', IngredientViewSet, basename="Ingredient")
+
 
 urlpatterns = [
     path('', include('djoser.urls')),
--- itog/posts/views.py
+++ itog/posts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Ingredient
+from .serializers import IngredientSerializer
 
-# Create your views here.
+
+class IngredientViewSet(ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+
+import json
+with open('../data/ingredients.json', encoding='utf-8') as f:
+    data = json.load(f)
+    for i in data:
+        print(i)
+
+        so = Ingredient(**i)
+        print(so)
+        #so.save()
