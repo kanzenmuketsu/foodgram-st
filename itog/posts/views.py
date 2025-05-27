7b80ed711349c1c41b613e0abbb1fc76b7ecbac7
Knz <dimapodschibiakin@yandex.ru>
2025-05-27 16:46:02 +0700

    indgredients

--- itog/posts/views.py
+++ itog/posts/views.py
@@ -1,19 +1,13 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 from .models import Ingredient
 from .serializers import IngredientSerializer
 
 
-class IngredientViewSet(ModelViewSet):
-    queryset = Ingredient.objects.all()
+class IngredientViewSet(ReadOnlyModelViewSet):
     serializer_class = IngredientSerializer
 
-
-import json
-with open('../data/ingredients.json', encoding='utf-8') as f:
-    data = json.load(f)
-    for i in data:
-        print(i)
-
-        so = Ingredient(**i)
-        print(so)
-        #so.save()
+    def get_queryset(self):
+        name = self.request.query_params.get('name', False)
+        if name:
+            return Ingredient.objects.filter(name__istartswith=name)
+        return Ingredient.objects.all()
